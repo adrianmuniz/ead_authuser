a2c8ccd8556e0c2bb3db2345771c0fb10963d78b
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2023-05-01 20:15:12 -0300

    Iserindo logs no registerUser

--- src/main/java/com/ead/authuser/controllers/AutheticationController.java
+++ src/main/java/com/ead/authuser/controllers/AutheticationController.java
@@ -6,6 +6,7 @@ import com.ead.authuser.enums.UserType;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.BeanUtils;
@@ -18,12 +19,11 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "x", maxAge = 3600)
 @RequestMapping("/auth")
 public class AutheticationController {
-
-    Logger logger = LogManager.getLogger(AutheticationController.class);
 
     @Autowired
     UserService userService;
@@ -31,6 +31,7 @@ public class AutheticationController {
     @PostMapping("/signup")
     public ResponseEntity<Object> registerUser(@RequestBody @Validated(UserDTO.UserView.RegistrationPost.class)
                                                    @JsonView(UserDTO.UserView.RegistrationPost.class) UserDTO userDTO) {
+        log.debug("POST registerUser userDto received {} ", userDTO.toString());
         if(userService.existsByUsername(userDTO.getUsername())){
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is Already Taken!");
         }
@@ -44,16 +45,17 @@ public class AutheticationController {
         userModel.setCreationDate(LocalDateTime.now(ZoneId.of("UTC")));
         userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
         userService.save(userModel);
+        log.debug("POST registerUser userModel saved {} ", userModel.toString());
         return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
     }
 
     @GetMapping("/")
     public String index(){
-        logger.trace("TRACE");
-        logger.debug("DEBUG");
-        logger.info("INFO");
-        logger.warn("WARN");
-        logger.error("ERROR");
+        log.trace("TRACE");
+        log.debug("DEBUG");
+        log.info("INFO");
+        log.warn("WARN");
+        log.error("ERROR");
         return "Logging Spring Boot";
     }
 
