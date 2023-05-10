2c91127b6ab7fce6d0c14d37331c5966d62bd2a3
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2023-05-09 23:45:53 -0300

    Fix: Injeção UserCourseRepository

--- src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserCourseServiceImpl.java
@@ -1,16 +1,16 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.repositories.UserCourseRepository;
 import com.ead.authuser.services.UserCourseService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserCourseServiceImpl implements UserCourseService {
 
     final
-    UserCourseService userCourseService;
+    UserCourseRepository userCourseRepository;
 
-    public UserCourseServiceImpl(UserCourseService userCourseService) {
-        this.userCourseService = userCourseService;
+    public UserCourseServiceImpl(UserCourseRepository userCourseRepository) {
+        this.userCourseRepository = userCourseRepository;
     }
 }
