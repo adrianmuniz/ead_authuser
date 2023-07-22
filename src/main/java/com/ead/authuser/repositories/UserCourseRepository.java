4153a2e51ea01192f7ed40ceefd387a3786df83d
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2023-07-22 15:21:32 -0300

    Deleção entre microservices parte 2

--- src/main/java/com/ead/authuser/repositories/UserCourseRepository.java
+++ src/main/java/com/ead/authuser/repositories/UserCourseRepository.java
@@ -3,7 +3,10 @@ package com.ead.authuser.repositories;
 import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserCourseRepository extends JpaRepository<UserCourseModel, UUID> {
@@ -11,6 +14,9 @@ public interface UserCourseRepository extends JpaRepository<UserCourseModel, UUI
     boolean existsByuserAndCourseId(UserModel userModel, UUID courseId);
 
     boolean existsByCourseId(UUID courseId);
+
+    @Query(value="select * from tb_users_courses where user_user_id = :userId", nativeQuery = true)
+    List<UserCourseModel> findAllUserCourseIntoUser(@Param("userId") UUID userId);
 
     void deleteAllByCourseId(UUID couseId);
 }
