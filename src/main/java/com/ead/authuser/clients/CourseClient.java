19b0442c326c7eddcbf2b7a112df5200d4006e65
Pedro Gabriel <144962807+PedroVecs@users.noreply.github.com>
2023-09-25 19:34:05 -0300

    Implementar Circuit Breaker Resilience4j

--- src/main/java/com/ead/authuser/clients/CourseClient.java
+++ src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -3,6 +3,7 @@ package com.ead.authuser.clients;
 import com.ead.authuser.dto.CourseDto;
 import com.ead.authuser.dto.ResponsePageDto;
 import com.ead.authuser.services.impl.UtilsServiceImpl;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -33,12 +35,15 @@ public class CourseClient {
     @Value("${ead.api.url.course}")
     String REQUEST_URL_COURSE;
 
-    @Retry(name = "retryInstance")
+
+    //@Retry(name = "retryInstance")
+    @CircuitBreaker(name = "circuitbreakerInstance")
     public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable){
         List<CourseDto> searchResult = null;
         String url = REQUEST_URL_COURSE + utilsService.createUrlGetAllCoursesByUser(userId, pageable);
         log.debug("Request URL: {} ", url);
         log.info("Request URL: {} ", url);
+        log.info("-- Tentando comunicar com o micro -- ");
         try{
             ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
             ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
@@ -48,6 +53,12 @@ public class CourseClient {
             log.error("Error request /courses {} ", e);
         }
         log.info("Ending request /courses userId {} ", userId);
+        return new PageImpl<>(searchResult);
+    }
+
+    public Page<CourseDto> circuitBreakerFallback(UUID userId, Pageable pageable, Throwable t){
+        log.error("Inside circuit breaker fallback, cause - {}", t.toString());
+        List<CourseDto> searchResult = new ArrayList<>();
         return new PageImpl<>(searchResult);
     }
 }
