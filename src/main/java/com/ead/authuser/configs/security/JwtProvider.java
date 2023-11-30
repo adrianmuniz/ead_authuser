e4b762db83c5a919bc5bd9aba31c3fd374d869b2
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2023-11-29 22:32:56 -0300

    Adicionando ROLES no Token JWT

--- src/main/java/com/ead/authuser/configs/security/JwtProvider.java
+++ src/main/java/com/ead/authuser/configs/security/JwtProvider.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Component
@@ -20,8 +21,14 @@ public class JwtProvider {
 
     public String generateJwt(Authentication authentication) {
         UserDetails userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+        final String roles = userPrincipal.getAuthorities().stream()
+                .map(role -> {
+                    return role.getAuthority();
+                }).collect(Collectors.joining(","));
+
         return Jwts.builder()
                 .setSubject((userPrincipal.getUsername()))
+                .claim("roles",roles)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
